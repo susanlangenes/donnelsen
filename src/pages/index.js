9c425600b1959762cd7d8bec0927df081a13eff3
Susan Langenes <susan@collagecreative.net>
2020-01-31 15:21:28 -0800

    work > article on index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,18 +7,18 @@ import Layout from "../components/layout"
 const IndexPage = ({ data }) => (
   <Layout>
     <Masonry className="showcase">
-      {data.allDatoCmsWork.edges.map(({ node: work }) => (
-        <div key={work.id} className="showcase__item">
+      {data.allDatoCmsArticle.edges.map(({ node: article }) => (
+        <div key={article.id} className="showcase__item">
           <figure className="card">
-            <Link to={`/works/${work.slug}`} className="card__image">
-              <Img fluid={work.coverImage.fluid} />
+            <Link to={`/articles/${article.slug}`} className="card__image">
+              <Img fluid={article.coverImage.fluid} />
             </Link>
             <figcaption className="card__caption">
               <h6 className="card__title">
-                <Link to={`/works/${work.slug}`}>{work.title}</Link>
+                <Link to={`/articles/${article.slug}`}>{article.title}</Link>
               </h6>
               <div className="card__description">
-                <p>{work.excerpt}</p>
+                <p>{article.excerpt}</p>
               </div>
             </figcaption>
           </figure>
@@ -32,7 +32,7 @@ export default IndexPage
 
 export const query = graphql`
   query IndexQuery {
-    allDatoCmsWork(sort: { fields: [position], order: ASC }) {
+    allDatoCmsArticle(sort: { fields: [position], order: ASC }) {
       edges {
         node {
           id
